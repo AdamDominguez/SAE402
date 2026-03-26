67a3c7b2b295310b5fabf7cdabfec71c9991ca6c
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2026-03-26 14:21:22 +0100

    feat!: initial game engine with canvas

--- assets/js/steering.js
+++ assets/js/steering.js
@@ -1,4 +1,13 @@
 let volant = document.querySelector('.buggatiroyale_wheel');
+let canvas = document.getElementById('gameCanvas');
+let ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let currentRotation = 0;
+let roadShiftX = 0;
+let forwardSpeedY = 0;
 
 window.addEventListener("click", handleClick, { once: true });
 
@@ -8,15 +17,51 @@ function handleClick() {
             .then(permissionState => {
                 if (permissionState === "granted") {
                     window.addEventListener("deviceorientation", handleOrientation);
+                    requestAnimationFrame(gameLoop);
                 }
             })
             .catch(console.error);
     } else {
         window.addEventListener("deviceorientation", handleOrientation);
+        requestAnimationFrame(gameLoop);
     }
 }
 
 function handleOrientation(event) {
-    let rotation = event.gamma;
-    volant.style.transform = `rotate(${rotation}deg)`;
+    currentRotation = event.gamma || 0;
+    volant.style.transform = `rotate(${currentRotation}deg)`;
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    roadShiftX -= currentRotation * 0.15;
+
+    let maxShift = canvas.width / 1.5;
+    if (roadShiftX > maxShift) roadShiftX = maxShift;
+    if (roadShiftX < -maxShift) roadShiftX = -maxShift;
+
+    forwardSpeedY += 0.08;
+
+    let horizonY = canvas.height * 0.45;
+    let horizonX = (canvas.width / 2) + roadShiftX;
+
+    ctx.fillStyle = "#87CEEB";
+    ctx.fillRect(0, 0, canvas.width, horizonY);
+
+    ctx.fillStyle = "#2b8a3e";
+    ctx.fillRect(0, horizonY, canvas.width, canvas.height - horizonY);
+
+    let roadTopWidth = 300;
+    let roadBottomWidth = canvas.width * 2;
+
+    ctx.fillStyle = "#343a40";
+    ctx.beginPath();
+    ctx.moveTo(horizonX - (roadTopWidth / 2), horizonY);
+    ctx.lineTo(horizonX + (roadTopWidth / 4), horizonY);
+    ctx.lineTo((canvas.width / 2) + (roadBottomWidth / 2), canvas.height);
+    ctx.lineTo((canvas.width / 2) - (roadBottomWidth / 2), canvas.height);
+    ctx.fill();
+    ctx.fillStyle = "white";
+
+    requestAnimationFrame(gameLoop);
 }
